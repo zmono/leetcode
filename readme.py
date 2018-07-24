b9f57fa83142566d474b5eae4ee01ab35d57ae70
zmono <kbkbwwb@yandex.com>
2018-07-24 21:29:35 +0500

    readme.py: ignore non-ready problems

--- readme.py
+++ readme.py
@@ -2,13 +2,15 @@
 I use this script to update README.md.
 """
 
+import subprocess
 from pathlib import Path
 
 
 def generate_problems_list(path='.'):
+    gitstatus = subprocess.run(['git', 'status'], capture_output=True)
     problems = []
     for p in  Path(path).iterdir():
-        if not p.is_dir() or p.name[0] == '.': continue
+        if not p.is_dir() or p.name[0] == '.' or p.name in gitstatus.stdout.decode(): continue
         with open(p / 'main.go') as f:
             title = next(f).strip('// ').strip()
             url = next(f).strip('// ').strip()
